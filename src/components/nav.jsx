d0725a17332305f522d5681c78ce18ff82789fea
MashtalerOlha <87094132+MashtalerOlha@users.noreply.github.com>
2022-09-09 22:29:14 +0300

    Update nav.jsx

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,29 +1,62 @@
 import Logo from "../images/logo.png";
 import UserSvg from "../images/symbol-defs.svg";
+import { Link } from "react-router-dom";
 
 export const Nav = () => {
   return (
     <>
       <div className="nav-container">
         <nav className="navigation">
-          <img src={Logo} width='130' height='22' alt="logo"className="header-logo" />
+          <img
+            src={Logo}
+            width="130"
+            height="22"
+            alt="logo"
+            className="header-logo"
+          />
           <ul className="nav-list">
-            <li className="nav-list__item">About Us</li>
-            <li className="nav-list__item">Courses</li>
-            <li className="nav-list__item">Events</li>
-            <li className="nav-list__item">Blog</li>
-            <li className="nav-list__item">Contacts</li>
+            <li className="nav-list__item">
+              <Link to="#" className="nav-link">
+                About Us
+              </Link>
+            </li>
+            <li className="nav-list__item">
+              <Link to="#" className="nav-link">
+                Courses
+              </Link>
+            </li>
+            <li className="nav-list__item">
+              <Link to="#" className="nav-link">
+                Events
+              </Link>
+            </li>
+            <li className="nav-list__item">
+              <Link to="#" className="nav-link">
+                Blog
+              </Link>
+            </li>
+            <li className="nav-list__item">
+              <Link to="#" className="nav-link">
+                Contacts
+              </Link>
+            </li>
           </ul>
         </nav>
 
         <div className="btn-menu">
-          <button type="button" className="header-button gradient" > Get consultation</button>
+          <button type="button" className="header-button gradient">
+            Get consultation
+          </button>
           <svg width="20" height="20">
             <use href={`${UserSvg}#icon-profile`} />
           </svg>
-          <button type="button" className="header-button__auth">Log in </button>
+          <button type="button" className="header-button__auth">
+            Log in
+          </button>
           <span className="header-span">/</span>
-          <button type="button" className="header-button__auth">Register</button>
+          <button type="button" className="header-button__auth">
+            Register
+          </button>
         </div>
       </div>
     </>
