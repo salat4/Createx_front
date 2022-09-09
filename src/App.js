bc850378016ce94a62c173c650eed7824ec13224
MashtalerOlha <87094132+MashtalerOlha@users.noreply.github.com>
2022-09-09 22:49:01 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import "./styles/variables.css";
-import { HeroHeader } from "./components/hero-header";
-
-import {Footer} from "./components/footer"
-
 import { Route, Routes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Start } from "./pages/start";
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<HeroHeader />} />
-      <Route path="/" element={<Footer />} />
+      <Route path="/" element={<Start />} />
 
     </Routes>
     </BrowserRouter>
