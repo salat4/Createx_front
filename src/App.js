d47bc9a2d6f28044e6c5bd427bf6e6ca4f70a1f4
MashtalerOlha <87094132+MashtalerOlha@users.noreply.github.com>
2022-09-09 22:31:07 +0300

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./styles/variables.css";
 import { HeroHeader } from "./components/hero-header";
+import { Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
 function App() {
-  return <HeroHeader/>;
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HeroHeader />} />
+    </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
