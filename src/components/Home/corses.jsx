85a133ff3de65ae3b929d0937857b960675dd6c7
Dima Malik <45137601+salat4@users.noreply.github.com>
2022-10-05 19:30:18 +0300

    update

--- src/components/Home/corses.jsx
+++ src/components/Home/corses.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import getCourses from "../../API/getCourses";
 import getColor from "../getColor";
-import { scrollUp } from "../scroll";
 
 export const Corses = () => {
   const [courses, setCourses] = useState(null);
